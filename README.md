47602d7fae38d63f419077e1a8382d3d9278dd63
Sebastien Binet <binet@cern.ch>
2015-11-18 17:03:09 +0100

    all: first import

---
+++ README.md
@@ -0,0 +1,24 @@
+gh-mv-issues
+==========
+
+`gh-mv-issues` moves the issues of a `github` repository to another repository.
+
+## Installation
+
+```sh
+$> go get github.com/sbinet/gh-mv-issues
+```
+
+## Documentation
+
+```sh
+$> gh-mv-issues -help
+```
+
+## Example
+
+```sh
+$> gh-mv-issues github.com/user/my-repo github.com/our-organization/repo
+```
+
+
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/google/go-github/github"
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage of %[1]s:
+$ %[1]s <src-repo> <dst-repo>
+
+example:
+
+$ %[1]s github.com/user/my-repo github.com/orga/repo
+
+`,
+			os.Args[0],
+		)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() != 2 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	src := flag.Arg(0)
+	dst := flag.Arg(1)
+
+	fmt.Printf(
+		"gh: moving issues...\ngh:   from [%s]\ngh:   to   [%s]\n",
+		src,
+		dst,
+	)
+
+	client := github.NewClient(nil)
+
+	var srcIssues []github.Issue
+	{
+		toks := strings.SplitN(src, "/", 3)
+		_, resp, err := client.Repositories.Get(toks[1], toks[2])
+		if err != nil {
+			log.Fatalf("error: %v\n", err)
+		}
+		log.Printf("resp: %#v\n", resp)
+		issues, _, err := client.Issues.ListByRepo(
+			toks[1], toks[2],
+			&github.IssueListByRepoOptions{State: "all"},
+		)
+		if err != nil {
+			log.Fatalf("error: %v\n", err)
+		}
+		srcIssues = issues
+	}
+
+	for _, issue := range srcIssues {
+		fmt.Printf("### #%d %v (%s)\n", *issue.Number, *issue.Title, *issue.State)
+	}
+}
